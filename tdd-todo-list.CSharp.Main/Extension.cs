6cc51778f00c8448945555560bbeb44eb68b9cc3
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-01-16 09:51:49 +0100

    failed code to examine

--- tdd-todo-list.CSharp.Main/Extension.cs
+++ tdd-todo-list.CSharp.Main/Extension.cs
@@ -12,28 +12,18 @@ namespace tdd_todo_list.CSharp.Main
 
         public List<ToDoTask> Tasks { get { return _tasks; } set { _tasks = value; } }
 
-        public void Add(int id, string name)
+
+        //This creates then adds a new task into the list.
+        public void Add(string name)
         {
-            ToDoTask newTask = new ToDoTask(id, name);
+            ToDoTask newTask = new ToDoTask(name);
             Tasks.Add(newTask);
         }
 
-        public string getCreationTime(int id)
-        {
-            string date = "01.01.2000 00:00:00";
-            foreach (var task in Tasks)
-            {
-                if (task.TaskID == id)
-                {
-                    date = task.Created.ToString();
-                }
-            }
-            return date;
-        }
-
+        //This is the method that we looked at.
         public ToDoTask getTaskByID(int id)
         {
-            ToDoTask returnedTask = new ToDoTask(0, "not found");
+            ToDoTask returnedTask = new ToDoTask("not found");
             foreach (var task in Tasks)
             {
                 if (task.TaskID == id)
@@ -44,19 +34,24 @@ namespace tdd_todo_list.CSharp.Main
             return returnedTask;
         }
 
-        public bool setStatusByID(int id, bool isCompleted)
+
+
+
+
+
+
+
+
+
+        public DateTime getCreationTime(int id)
         {
-            bool statusChanged = false;
-            ToDoTask returnedTask = new ToDoTask(0, "not found");
-            foreach (var task in Tasks)
-            {
-                if (task.TaskID == id)
-                {
-                    task.IsCompleted = isCompleted;
-                    statusChanged = true;
-                }
-            }
-            return statusChanged;
+            throw new NotImplementedException();
+        }
+
+
+        public void setStatusByID(int id, bool isCompleted)
+        {
+            throw new NotImplementedException();
         }
 
         public string updateName(int id, string newName)
@@ -77,25 +72,25 @@ namespace tdd_todo_list.CSharp.Main
 
     public class ToDoTask
     {
-        //private static int _idCounter; This sadly did not work :C I wanted it to automatically iterate
+        private static int _idCounter;
         private int _taskID;
         private string _taskName;
         private bool _isCompleted;
         private DateTime _created = new DateTime();
 
-        //public static int IDCounter { get { return _idCounter; } set { _idCounter = value; } }
+        public static int IDCounter { get { return _idCounter; } set { _idCounter = value; } }
         public int TaskID { get { return _taskID; } set { _taskID = value; } }
         public string TaskName { get { return _taskName; } set { _taskName = value; } }
         public bool IsCompleted { get { return _isCompleted; } set { _isCompleted = value; } }
         public DateTime Created { get { return _created; } set { _created = value; } }
 
-        public ToDoTask(int id, string taskName)
+        public ToDoTask(string taskName)
         {
-            //TaskID = ++IDCounter;
-            TaskID = id; //I have to manually set the ID (boring!)
+            TaskID = IDCounter++;
             TaskName = taskName;
             IsCompleted = false;
             Created = DateTime.Now;
         }
+
     }
 }
--- tdd-todo-list.CSharp.Test/ExtensionTests.cs
+++ tdd-todo-list.CSharp.Test/ExtensionTests.cs
@@ -10,16 +10,27 @@ namespace tdd_todo_list.CSharp.Test
 {
     public class ExtensionTests
     {
+        /*
+        private TodoListExtension Extension;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Extension = new TodoListExtension();
+        }
+        */
+
+        //THIS ONE THAT WE LOOKED AT
         [Test, Order(1)]
         [TestCase(1, "Achieving CHIM")]
         [TestCase(2, "Banana eating contest")]
         [TestCase(3, "Calculate all float values of pi")]
         [TestCase(4, "Destroy the universe")]
-        public void getTaskByIDTest (int id, string name)
+        public void getTaskByIDTest(int id, string name)
         {
             //Arrange
             TodoListExtension Extension = new TodoListExtension();
-            Extension.Add(id,name);
+            Extension.Add(name);
 
             //Act
             ToDoTask returnedTask = Extension.getTaskByID(id);
@@ -29,18 +40,35 @@ namespace tdd_todo_list.CSharp.Test
 
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         [Test, Order(2)]
         [TestCase(666)]
-        [TestCase(0)]
-        [TestCase(5)]
         public void getTaskByIDTestFail(int id)
         {
             //Arrange
             TodoListExtension Extension = new TodoListExtension();
-            Extension.Add(1, "Banana eating contest");
-            Extension.Add(2, "Achieving CHIM");
-            Extension.Add(3, "Destroy the universe");
-            Extension.Add(4, "Calculate all float values of pi");
+            Extension.Add("Banana eating contest");
+            Extension.Add("Achieving CHIM");
+            Extension.Add("Destroy the universe");
+            Extension.Add("Calculate all float values of pi");
 
             //Act
             ToDoTask returnedTask = Extension.getTaskByID(id);
@@ -50,87 +78,72 @@ namespace tdd_todo_list.CSharp.Test
         }
 
         [Test, Order(3)]
-        [TestCase(1, "Eat a lot of pizza")]
-        [TestCase(2, "Forget how to ride a bike")]
-        [TestCase(3, "Go home for the day")]
-        [TestCase(4, "Heat up leftover pasta")]
+        [TestCase(3, "Eat a lot of pizza")]
         public void updateNameTest(int id, string name)
         {
             //Arrange
             TodoListExtension Extension = new TodoListExtension();
-            Extension.Add(1, "Banana eating contest");
-            Extension.Add(2, "Achieving CHIM");
-            Extension.Add(3, "Destroy the universe");
-            Extension.Add(4, "Calculate all float values of pi");
+            Extension.Add("Banana eating contest");
+            Extension.Add("Achieving CHIM");
+            Extension.Add("Destroy the universe");
+            Extension.Add("Calculate all float values of pi");
 
             //Act
             string result = Extension.updateName(id, name);
 
             //Assert
-            Assert.That(result, Is.EqualTo(name));
+            Assert.That(result, Is.EqualTo("Eat a lot of pizza"));
         }
 
         [Test, Order(4)]
-        [TestCase(1, true)]
-        [TestCase(2, true)]
-        [TestCase(3, true)]
-        [TestCase(4, true)]
         public void setStatusByIDTest(int id, bool isCompleted)
         {
+
             //Arrange
             TodoListExtension Extension = new TodoListExtension();
-            Extension.Add(1, "Banana eating contest");
-            Extension.Add(2, "Achieving CHIM");
-            Extension.Add(3, "Destroy the universe");
-            Extension.Add(4, "Calculate all float values of pi");
+            Extension.Add("Banana eating contest");
+            Extension.Add("Achieving CHIM");
+            Extension.Add("Destroy the universe");
+            Extension.Add("Calculate all float values of pi");
             //Act
-            bool hasChanged = Extension.setStatusByID(id, isCompleted);
+            Extension.setStatusByID(id, isCompleted);
 
             //Assert
-            Assert.That(hasChanged, Is.EqualTo(true));
+            Assert.Fail();
         }
 
         [Test, Order(5)]
-        [TestCase(666, true)]
-        [TestCase(0, true)]
-        [TestCase(5, true)]
         public void setStatusByIDTestFail(int id, bool isCompleted)
         {
 
             //Arrange
             TodoListExtension Extension = new TodoListExtension();
-            Extension.Add(1, "Banana eating contest");
-            Extension.Add(2, "Achieving CHIM");
-            Extension.Add(3, "Destroy the universe");
-            Extension.Add(4, "Calculate all float values of pi");
+            Extension.Add("Banana eating contest");
+            Extension.Add("Achieving CHIM");
+            Extension.Add("Destroy the universe");
+            Extension.Add("Calculate all float values of pi");
             //Act
-            bool hasChanged = Extension.setStatusByID(id, isCompleted);
+            Extension.setStatusByID(id, isCompleted);
 
             //Assert
-            Assert.That(hasChanged, Is.EqualTo(false));
+            Assert.Fail();
         }
 
         [Test, Order(6)]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
         public void getCreationTimeTest(int id)
         {
 
             //Arrange
             TodoListExtension Extension = new TodoListExtension();
-            Extension.Add(1, "Banana eating contest");
-            Extension.Add(2, "Achieving CHIM");
-            Extension.Add(3, "Destroy the universe");
-            Extension.Add(4, "Calculate all float values of pi");
-
+            Extension.Add("Banana eating contest");
+            Extension.Add("Achieving CHIM");
+            Extension.Add("Destroy the universe");
+            Extension.Add("Calculate all float values of pi");
             //Act
-            string thisSecondHopefully = DateTime.Now.ToString(); //Of course a chance to fail, but it's highly unlikely
-            string returnedDate = Extension.getCreationTime(id);
+            Extension.getCreationTime(id);
 
             //Assert
-            Assert.That(returnedDate, Is.EqualTo(thisSecondHopefully));
+            Assert.Fail();
         }
     }
 }
